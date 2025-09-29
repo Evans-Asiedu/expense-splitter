2569606ff2ae346e4f738b26168de3c586eed801
Evans-Asiedu <evansasiedu352@gmail.com>
2025-09-29 19:11:13 +0000

    Refactor Form component  and build separate Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,21 @@
 interface Props {
   name: string;
   type: string;
-  value: string;
+  value?: string;
   placeholder: string;
-  onChange: () => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  className?: string;
 }
-const Input = ({ name, type, value, placeholder, onChange }: Props) => {
+const Input = ({
+  name,
+  type,
+  value,
+  placeholder,
+  onChange,
+  onFocus,
+  className,
+}: Props) => {
   return (
     <div className="mt-2 mb-7 ">
       <input
@@ -14,7 +24,8 @@ const Input = ({ name, type, value, placeholder, onChange }: Props) => {
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="py-1 px-2 w-full rounded-md outline-1 outline-gray-300 placeholder:text-accent-txt placeholder:text-sm focus:outline-2 focus:outline-primary"
+        className={`py-1 px-2 w-full rounded-md outline-1 outline-gray-300 placeholder:text-accent-txt placeholder:text-sm focus:outline-2 focus:outline-primary ${className}`}
+        onFocus={onFocus}
       />
     </div>
   );
