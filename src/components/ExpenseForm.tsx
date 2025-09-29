e94b27887c70fcd7f9124471d9321beb971eeb79
Evans-Asiedu <evansasiedu352@gmail.com>
2025-09-29 19:12:01 +0000

    Add functionality to ExpenseForm component

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,17 +1,258 @@
+import { useEffect, useState } from "react";
 import type { Expense } from "../models/GroupModels";
 import Button from "./Button";
 import Header from "./Header";
 import Input from "./Input";
 
+type Participant = {
+  id: string;
+  name: string;
+  contribution: string;
+  avatarUrl: string;
+  weight: number;
+};
+
+export type ExpenseData = {
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  receipt: string;
+  amount: number;
+  expenseParticipants: Participant[];
+};
+
+type errorData = {
+  name: string;
+  message: string;
+};
+
 interface Props {
-  groupId: string;
-  onExpenseAdded: () => void;
+  onSubmit: (expense: ExpenseData) => void;
   onClose: () => void;
-  expense?: Expense;
+  expense?: Expense | null;
 }
 
-const ExpenseForm = ({ groupId, onExpenseAdded, onClose, expense }: Props) => {
-  console.log(groupId, onExpenseAdded, onClose, expense);
+const ExpenseForm = ({ onSubmit, onClose, expense }: Props) => {
+  //console.log(groupId);
+
+  const [formData, setFormData] = useState<ExpenseData>({
+    id: "",
+    name: "",
+    description: "",
+    date: "",
+    receipt: "",
+    amount: 0,
+    expenseParticipants: [],
+  });
+  const [splitEvenly, setSplitEvenly] = useState<boolean>(true);
+  const [participantName, setParticipantName] = useState<string>("");
+  const [participantContribution, setParticipantContribution] =
+    useState<string>("");
+  const [splitEvenWeight, setSplitEvenWeight] = useState<number>(100);
+
+  const [error, setError] = useState<errorData>({
+    name: "",
+    message: "",
+  });
+
+  const handleSplitWeightEvenly = () => {
+    const value =
+      formData.expenseParticipants.length > 0
+        ? 100 / formData.expenseParticipants.length
+        : 100;
+
+    setSplitEvenWeight(Number(value.toFixed(2)));
+  };
+
+  useEffect(() => {
+    // handleSplitWeightEvenly();
+    const value =
+      formData.expenseParticipants.length > 0
+        ? 100 / formData.expenseParticipants.length
+        : 100;
+
+    setSplitEvenWeight(Number(value.toFixed(2)));
+  }, [formData.expenseParticipants]);
+
+  useEffect(() => {
+    if (expense) {
+      setFormData({
+        id: expense.id,
+        name: expense.name,
+        description: expense.description,
+        date: expense.date,
+        receipt: expense.receipt,
+        amount: expense.amount,
+        expenseParticipants: [...expense.expenseParticipants],
+      });
+    }
+
+    //  handleSplitWeightEvenly();
+  }, [expense]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { name, amount, expenseParticipants, receipt, description } =
+      formData;
+
+    if (!name.trim()) {
+      setError({
+        name: "name",
+        message: "Expense name is required",
+      });
+
+      return;
+    }
+
+    if (!amount) {
+      setError({ name: "amount", message: "expense amount is required" });
+      return;
+    }
+
+    const expenseAmount = Number(amount);
+    if (expenseAmount <= 0 || isNaN(expenseAmount)) {
+      setError({
+        name: "amount",
+        message: "amount must be a valid value and greater than 0",
+      });
+      return;
+    }
+
+    // updated participant list;
+    const updatedParticipants = expenseParticipants.map((participant) => {
+      return {
+        ...participant,
+        weight: splitEvenly ? splitEvenWeight : participant.weight,
+      };
+    });
+
+    const newExpense: ExpenseData = {
+      id: expense ? expense.id : Date.now().toString(),
+      name: name,
+      description: description,
+      date: new Date(Date.now()).toISOString().split("T")[0],
+      receipt: receipt ? receipt : expense?.receipt || "",
+      amount: expenseAmount,
+      expenseParticipants: updatedParticipants,
+    };
+
+    onSubmit(newExpense);
+    onClose();
+  };
+
+  const handleWeightChange = (index: number, value: number) => {
+    if (value > 100 || value < 0) {
+      value = value < 0 ? 0 : 100;
+    }
+
+    const updatedParticipants = formData.expenseParticipants;
+    updatedParticipants[index].weight = value;
+    setFormData((prev) => ({
+      ...prev,
+      expenseParticipants: updatedParticipants,
+    }));
+  };
+
+  const handleReceiptChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    setFormData((prev) => ({
+      ...prev,
+      receipt: file ? file.name : "",
+    }));
+  };
+
+  const addParticipant = () => {
+    if (!participantName.trim() || participantName.length <= 2) {
+      setError({
+        name: "participantName",
+        message: "Person's name is required and must be more two characters",
+      });
+      return;
+    }
+
+    if (!participantContribution || isNaN(Number(participantContribution))) {
+      setError({
+        name: "contributedAmount",
+        message: "Contribution must be a valid number",
+      });
+      return;
+    }
+
+    if (Number(participantContribution) < 0) {
+      setError({
+        name: "contributedAmount",
+        message: "Contribution cannot be a negative value",
+      });
+
+      return;
+    }
+
+    setError({ name: "", message: "" });
+
+    console.log("new particpants");
+    console.log(participantName, participantContribution);
+
+    const newParticipant: Participant = {
+      id: Date.now().toString(),
+      name: participantName,
+      contribution: participantContribution,
+      weight: 0,
+      avatarUrl:
+        "https://cdn.usegalileo.ai/stability/e995b051-eff4-4ba3-964c-7ebd7b82b9dc.png",
+    };
+
+    console.log("new partnicipant", newParticipant);
+
+    setFormData((prev) => ({
+      ...prev,
+      expenseParticipants: [...prev.expenseParticipants, newParticipant],
+    }));
+
+    setParticipantName("");
+    setParticipantContribution("");
+  };
+
+  const removeParticipant = (index: number) => {
+    setFormData((prev) => ({
+      ...prev,
+      expenseParticipants: prev.expenseParticipants.filter(
+        (_, i) => i !== index
+      ),
+    }));
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      setFormData((prev) => ({
+        ...prev,
+        name: value,
+      }));
+    }
+
+    if (name === "amount") {
+      setFormData((prev) => ({
+        ...prev,
+        amount: Number(value),
+      }));
+    }
+
+    if (name === "description") {
+      setFormData((prev) => ({
+        ...prev,
+        description: value,
+      }));
+    }
+  };
+
+  const handleFocus = () => {
+    setError({ name: "", message: "" });
+  };
 
   return (
     <div className="w-full p-4 fixed inset-0 bg-black/40 flex items-center justify-center z-50">
@@ -19,30 +260,42 @@ const ExpenseForm = ({ groupId, onExpenseAdded, onClose, expense }: Props) => {
         className="p-4 border-2 bg-secondary-txt rounded-md
                     max-h-[80vh] overflow-y-auto"
       >
-        <Header title="Add Expense" />
-        <form action="" className="relative">
+        <Header title={expense ? "Edit Expense" : "Add Expense"} />
+        <form action="" onSubmit={handleSubmit} className="mt-7">
+          <div>
+            {error.name === "name" && (
+              <div className="bg-red-300 p-1 text-sm">{error.message}</div>
+            )}
+          </div>
           <Input
             name="name"
             type="text"
-            value=""
-            onChange={() => {}}
+            value={formData.name}
+            onChange={handleInputChange}
             placeholder="Name"
+            onFocus={handleFocus}
           />
 
+          <div>
+            {error.name === "amount" && (
+              <div className="bg-red-300 p-1 text-sm">{error.message}</div>
+            )}
+          </div>
           <Input
             name="amount"
             type="number"
-            value=""
-            onChange={() => {}}
+            value={formData.amount ? formData.amount.toString() : ""}
+            onChange={handleInputChange}
             placeholder="Amount"
+            onFocus={handleFocus}
           />
 
           <div className="">
             <textarea
               name="description"
               rows={3}
-              value=""
-              onChange={() => {}}
+              value={formData.description}
+              onChange={handleInputChange}
               placeholder="Description"
               className="p-2 block w-full outline-1 outline-gray-300 rounded-md placeholder:text-accent-txt placeholder:text-sm focus:outline-2 focus:outline-primary"
             />
@@ -52,32 +305,99 @@ const ExpenseForm = ({ groupId, onExpenseAdded, onClose, expense }: Props) => {
             <input
               name="splitevenly"
               type="checkbox"
-              value=""
-              onChange={() => {}}
+              checked={splitEvenly}
+              onChange={() => {
+                handleSplitWeightEvenly();
+                setSplitEvenly(!splitEvenly);
+              }}
               className="mr-3 rounded-md outline-1 outline-gray-300 focus:outline-2 focus:outline-primary"
             />
             <label htmlFor="">Split evenly among Participant</label>
           </div>
 
           <div>
-            <label htmlFor="">Participants</label>
-            <div className="flex items-start gap-x-3">
+            <label htmlFor="" className="text-lg font-semibold">
+              Participants
+            </label>
+            <div>
+              {formData.expenseParticipants.map((participant, index) => (
+                <div className="px-2 pt-2 pb-6 border-b-1 border-b-gray-300 flex items-start">
+                  <span className="h-12 w-12 mr-3">
+                    <img
+                      src={participant.avatarUrl}
+                      alt=""
+                      className="rounded-full"
+                    />
+                  </span>
+                  <div className="text-xs h-12 ">
+                    <p className="font-bold">{participant.name}</p>
+                    <p>Contribution: $ {participant.contribution}</p>
+                    {splitEvenly ? (
+                      <p>Weight (%): {splitEvenWeight}</p>
+                    ) : (
+                      <div className="flex gap-x-2 items-center">
+                        <p className="h-1">Weight (%): </p>
+                        <div className="w-14 h-6">
+                          <Input
+                            name="contribution"
+                            type="number"
+                            value={participant.weight.toString()}
+                            placeholder="weight"
+                            onChange={(e) => {
+                              handleWeightChange(index, Number(e.target.value));
+                            }}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <span
+                    className="ml-auto hover:cursor-pointer"
+                    onClick={() => removeParticipant(index)}
+                  >
+                    <i
+                      className="fa fa-times text-red-400"
+                      aria-hidden="true"
+                    ></i>
+                  </span>
+                </div>
+              ))}
+            </div>
+            <div>
+              {["participantName", "contributedAmount"].includes(
+                error.name
+              ) && (
+                <div className="bg-red-300 p-1 text-sm">{error.message}</div>
+              )}
+            </div>
+            <div className="flex items-start gap-x-3 mt-1">
               <Input
                 name="participantName"
                 type="text"
-                value=""
-                onChange={() => {}}
+                value={participantName}
+                onChange={(e) => {
+                  setParticipantName(e.target.value);
+                }}
                 placeholder="Participant Name"
+                onFocus={handleFocus}
               />
               <Input
                 name="contributedAmount"
                 type="number"
-                value=""
-                onChange={() => {}}
+                value={participantContribution}
+                onChange={(e) => {
+                  setParticipantContribution(e.target.value);
+                }}
                 placeholder="Contributed Amount"
+                onFocus={handleFocus}
               />
               <span className="pt-1.5">
-                <Button name="Add" isPrimary={true} />
+                <Button
+                  name="Add"
+                  isPrimary={true}
+                  type="button"
+                  onClick={addParticipant}
+                />
               </span>
             </div>
           </div>
@@ -85,17 +405,17 @@ const ExpenseForm = ({ groupId, onExpenseAdded, onClose, expense }: Props) => {
           <div>
             <label htmlFor="">Receipt</label>
             <Input
-              name="receipt"
+              name=""
               type="file"
-              value=""
-              onChange={() => {}}
+              onChange={handleReceiptChange}
               placeholder=""
+              className="file:border-1 file:border-black-200 file:px-2 file:mr-4"
             />
           </div>
 
           <div className="flex gap-2 mt-4">
-            <Button name="cancel" isPrimary={false} />
-            <Button name="save" isPrimary={true} />
+            <Button name="cancel" isPrimary={false} onClick={onClose} />
+            <Button name="save" isPrimary={true} type="submit" />
           </div>
         </form>
       </div>
--- src/components/pages/ExpensesPage.tsx
+++ src/components/pages/ExpensesPage.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import type { Expense } from "../../models/GroupModels";
 import ExpenseCard from "../ExpenseCard";
-import { getExpenses } from "../../services/ExpenseService";
+import { getExpenses, saveExpense } from "../../services/ExpenseService";
+import Button from "../Button";
+import ExpenseForm from "../ExpenseForm";
+import type { ExpenseData } from "../ExpenseForm";
 
 interface Props {
   groupId: string;
@@ -9,17 +12,77 @@ interface Props {
 
 const ExpensesPage = ({ groupId }: Props) => {
   const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [isFormOpen, setIsFormOpen] = useState<boolean>(false);
+  const [expense, setExpense] = useState<Expense | null>(null);
+  const [selectedExpenseId, setSelectedExpenseId] = useState<string>("");
+
+  const showForm = (expenseId: string | null) => {
+    if (expenseId === null) {
+      setIsFormOpen(true);
+      setExpense(null);
+      return;
+    }
+
+    setIsFormOpen(true);
+
+    const currentExpense = expenses.find((e) => e.id === expenseId);
+    setExpense(currentExpense ?? null);
+    // const currentExpense = expenses.find((e) => e.id === expenseId);
+    // setExpense(currentExpense ?? null);
+    setSelectedExpenseId(expenseId);
+  };
+
+  const handleSubmit = (expense: ExpenseData) => {
+    // handling new expenses or edit
+    console.log("expense:", expense);
+
+    setExpenses((prev) =>
+      prev.some((e) => e.id === expense.id)
+        ? prev.map((e) => (e.id === expense.id ? expense : e))
+        : [...prev, expense]
+    );
+
+    // save expense it to database;
+    saveExpense(groupId, expense);
+  };
+
+  const handleFormClose = () => {
+    setIsFormOpen(false);
+    setSelectedExpenseId("");
+  };
 
   useEffect(() => {
+    // get expense from data base;
     setExpenses(getExpenses(groupId));
   }, [groupId]);
 
-  console.log(groupId);
   return (
     <div>
       {expenses.map((expense) => (
-        <ExpenseCard expense={expense} />
+        <ExpenseCard
+          expense={expense}
+          onClickExpenseItem={showForm}
+          selectedId={selectedExpenseId}
+        />
       ))}
+
+      <div>
+        {isFormOpen && (
+          <ExpenseForm
+            onSubmit={(expense) => handleSubmit(expense)}
+            onClose={handleFormClose}
+            expense={expense}
+          />
+        )}
+      </div>
+
+      <div>
+        <Button
+          name="Add Expense"
+          isPrimary={true}
+          onClick={() => showForm(null)}
+        />
+      </div>
     </div>
   );
 };
