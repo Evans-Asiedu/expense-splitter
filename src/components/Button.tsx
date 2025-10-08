59ed2688ecdcb646506f961b6d85a9fdb9ccd420
Evans-Asiedu <evansasiedu352@gmail.com>
2025-10-08 20:40:20 +0000

    Update Button component to allow for buttons of different shapes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,7 @@ interface Props {
   isPrimary: boolean;
   type?: "button" | "submit" | "reset";
   color?: string;
+  rounded?: boolean;
 }
 
 const Button = ({
@@ -11,12 +12,30 @@ const Button = ({
   onClick,
   isPrimary,
   color,
+  rounded,
   type = "button",
 }: Props) => {
+  if (rounded) {
+    return (
+      <button
+        type={type}
+        className={`px-3.5 py-1 text-md rounded-full  hover:cursor-pointer ${
+          color
+            ? color
+            : isPrimary
+            ? "bg-primary text-secondary-txt hover:bg-sky-500"
+            : "bg-secondary text-primary-text hover:bg-gray-500 outline-1"
+        }`}
+        onClick={onClick}
+      >
+        {name}
+      </button>
+    );
+  }
   return (
     <button
       type={type}
-      className={`px-3 py-1.5 rounded-xl ${
+      className={`px-3 py-1.5 rounded-xl  hover:cursor-pointer ${
         color
           ? color
           : isPrimary
