e5091c68b584ca139d35d8e110da0cdbdec8b047
Evans-Asiedu <evansasiedu352@gmail.com>
2025-10-12 21:07:28 +0000

    Define animation effect and add to routes pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import CreateGroupPage from "./components/pages/CreateGroupPage";
 import GroupDetailsPage from "./components/pages/GroupDetailsPage";
 import GroupListPage from "./components/pages/GroupListPage";
 import HomePage from "./components/pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  const [fadeKey, setFadeKey] = useState(location.pathname);
+
+  // Update animation when route changes
+  useEffect(() => {
+    setFadeKey(location.pathname);
+  }, [location]);
+
+  return (
+    <div
+      key={fadeKey}
+      className="fade-in transition-all duration-500 ease-in-out"
+    >
+      <Routes location={location}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/groups" element={<GroupListPage />} />
+        <Route path="/groups/:id" element={<GroupDetailsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default function App() {
   return (
     <>
       <div className="min-h-screen bg-background">
         <BrowserRouter>
           <NavBar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/groups" element={<GroupListPage />} />
-            <Route path="/groups/:id" element={<GroupDetailsPage />} />
-            {/* <Route path="/analytics" element={} /> */}
-            <Route path="add-group" element={<CreateGroupPage />} />
-          </Routes>
+          <AnimatedRoutes />
         </BrowserRouter>
       </div>
     </>
   );
 }
 
-export default App;
+// export default App;
